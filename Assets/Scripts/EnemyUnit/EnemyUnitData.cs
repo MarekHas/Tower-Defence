86f3b0c45a9f6f2192786370d53f37a2fd2256b0
MarekHas <45779384+MarekHas@users.noreply.github.com>
2020-11-24 19:17:23 +0100

    enemy unit data

---
+++ Assets/Scripts/EnemyUnit/EnemyUnitData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New EnemyUnitData", menuName = "Enemy/EnemyUnitData")]
+public class EnemyUnitData : ScriptableObject
+{
+    [SerializeField] private int _damage = 10;
+    [SerializeField] private int _health = 10;
+    [SerializeField] private int _moneyForKill = 10;
+    [SerializeField] private float _speed = 5f;
+
+    public int Damage => _damage;
+    public int Health => _health;
+    public int MoneyForKill => _moneyForKill;
+    public float Speed => _speed;
+}
