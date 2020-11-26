bbbb47d8017adaa10ca2863cd82839293ab39b36
MarekHas <45779384+MarekHas@users.noreply.github.com>
2020-11-26 18:00:31 +0100

    main menu scene, load level , exit game scripts

---
+++ Assets/Scripts/Menu/LevelsMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelsMenu : MonoBehaviour
+{
+    private Button[] _levelButtons;
+
+    private void Start()
+    {
+        _levelButtons = GetComponentsInChildren<Button>();
+
+        int levelWithHighestIndex = PlayerPrefs.GetInt(GameOverHandler.HighestLevelIndex, 0);
+
+        for (int i = 0; i < levelWithHighestIndex + 1; i++)
+        {
+            _levelButtons[i].interactable = true;
+        }
+    }
+
+    public void LoadLevel(string name) => SceneManager.LoadScene(name);
+}
