b83216a84da119de627a22fbe24396d3a2004b0e
MarekHas <45779384+MarekHas@users.noreply.github.com>
2020-11-26 18:10:36 +0100

    level 1 prototype

---
+++ Assets/Scripts/EnemiesWave/PlayerBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerBase : MonoBehaviour
+{
+    public static event Action<EnemyUnitData> OnEnemyUnitGetToBase;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (!collider.TryGetComponent<EnemyUnit>(out var enemyUnit))
+        {
+            return;
+        }
+
+        OnEnemyUnitGetToBase?.Invoke(enemyUnit.EnemyUnitData);
+
+        Destroy(enemyUnit.gameObject);
+    }
+}
