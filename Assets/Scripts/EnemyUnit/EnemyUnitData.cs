547909faa355969c895fbe69084c2000dc571754
MarekHas <45779384+MarekHas@users.noreply.github.com>
2020-11-30 18:36:45 +0100

    paint terrain level 1

--- Assets/Scripts/EnemyUnit/EnemyUnitData.cs
+++ Assets/Scripts/EnemyUnit/EnemyUnitData.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New EnemyUnitData", menuName = "Enemy/EnemyUnitData")]
 public class EnemyUnitData : ScriptableObject
 {
+    [SerializeField] private string _name;
     [SerializeField] private int _damage = 10;
     [SerializeField] private int _health = 10;
     [SerializeField] private int _moneyForKill = 10;
